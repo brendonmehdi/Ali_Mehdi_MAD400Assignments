5febda5d587c63546687205fc280f45ae35949be
Brendon Mehdi <brendonmehdi@gmail.com>
2024-01-26 15:51:06 -0500

    Created Components

--- src/app/app.assignment2.ts
+++ src/app/app.assignment2.ts
@@ -1,3 +1,3 @@
-export class Assignment2 { 
-    constructor(public assign2Bool?: boolean, public assign2TextDef: string = 'default') {} 
-}
+// export class Assignment2 { 
+//     constructor(public assign2Bool?: boolean, public assign2TextDef: string = 'default') {} 
+// }
